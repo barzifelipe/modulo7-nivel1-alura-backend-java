a8c1a291675abbdf2c7087856701326d61648288
barzifelipe <barzilaifelipe@gmail.com>
2025-12-19 22:14:35 -0300

    Add: exercicio01

---
+++ exercicio01/src/Degrau.java
@@ -0,0 +1,21 @@
+/**/
+
+import java.util.Scanner;
+
+public class Degrau {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o número de degraus: ");
+        int nDegraus = scan.nextInt();
+        contador(nDegraus);
+
+    }
+
+    public static void contador(int nDegraus){
+        for (int i = 1; i <= nDegraus; i++) {
+            System.out.println("Subindo o degrau "+i);
+        }
+        System.out.println("Você chegou ao topo!");
+    }
+}
